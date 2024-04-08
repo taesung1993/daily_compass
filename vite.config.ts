1fd9c8bed4ff7126012a55c211cde2f636d2c3f0
윤태성 <masonyun@yuntaeseongs-MacBook-Pro.local>
2024-04-09 02:04:23 +0900

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/today_compass/",
   plugins: [react()],
   resolve: {
     alias: [
